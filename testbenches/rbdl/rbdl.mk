8fa11d243e43e252d1f0e91d27140428d644e61c
jmkang <mkang@eecs.berkeley.edu>
2021-04-05 23:05:56 +0000

    modify rbdl/rbdl.mk to support x86 compilation

--- testbenches/rbdl/rbdl.mk
+++ testbenches/rbdl/rbdl.mk
@@ -9,11 +9,18 @@ CXXFLAGS += -DRBD_BENCHMARKS_DIR=\"$(RBD_BENCHMARKS_PATH)\" -DRESULTS_DIR=\"$(RE
 CXXFLAGS += -I$(RBDL_PATH) -I$(RBDL_PATH)/include 
 #CXXFLAGS += -O3 -DNDEBUG -std=c++11
 CXXFLAGS += -O0 -g -DDEBUG -std=c++11
+
+#x86:
+#LDFLAGS += -L$(RBDL_PATH)/lib -lrbdl -lrbdl_urdfreader -lrt
+#LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib
+
+#RISC-V:
 LDFLAGS += -L$(RBDL_PATH)/lib64 -lrbdl -lrbdl_urdfreader -lrt
 LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib64
 
 
 $(TARGET): $(TARGET).o
+	echo $(RBD_BENCHMARKS_PATH)
 	$(CXX) $(TARGET).o -o $(TARGET) $(LDFLAGS)
 
 $(TARGET).o: $(TARGET).cc

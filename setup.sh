c33426698609c01de782b1bd3fefb041080b75cc
Vadim Nikiforov <vnikiforov@a5.Millennium.Berkeley.EDU>
2021-04-05 11:22:30 -0700

    add scripts for getting and unpacking csv zip, hosted on google drive

--- setup.sh
+++ setup.sh
@@ -2,4 +2,16 @@
 set -e
 
 git submodule update --init --recursive
+mkdir -p ./csv/
+mkdir -p ./csv/rbdl/
+
+# https://drive.google.com/u/1/uc?id=1AgACbjUpoWWA47U_RFR-jwIdt4lLTtdW&export=download
+fileId=1AgACbjUpoWWA47U_RFR-jwIdt4lLTtdW
+fileName=csvs.zip
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+
+unzip "${fileName}" -d ./csv/rbdl/
+rm "${fileName}"
 

6e834468a3975ce34dbb7487cfac59f9dee539af
Cloud User <centos@ip-192-168-0-183.ec2.internal>
2021-04-27 21:58:14 +0000

    update experiment build scripts to use riscv linux cross compiler

---
+++ experiments/rbdl/compile_rbdl_time.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# run_rbdl_time.sh
+# Usage: ./run_rbdl_time.sh
+RBD_BENCHMARKS_PATH=`realpath $(dirname "$0")/../..`
+RESULTS_PATH=$RBD_BENCHMARKS_PATH/results
+echo "Compiling time experiments..."
+for DATA_NUM in 0
+do
+   echo "($DATA_NUM) RBDL:"
+   cd $RBD_BENCHMARKS_PATH/testbenches/rbdl/time_rbdl
+   make clean
+   make RBD_BENCHMARKS=$RBD_BENCHMARKS_PATH RESULTS=$RESULTS_PATH
+cd $RBD_BENCHMARKS_PATH/experiments/rbdl
+done
+echo "Done!"
--- testbenches/rbdl/rbdl.mk
+++ testbenches/rbdl/rbdl.mk
@@ -4,6 +4,7 @@ RBDL_PATH=$(RBD_BENCHMARKS_PATH)/libs/rbdl/install
 EIGEN3_PATH=$(RBDL_PATH)/include/eigen3
 RESULTS_PATH=$(RESULTS)
 
+CXX=riscv64-unknown-linux-gnu-g++
 CXXFLAGS += -DRBD_BENCHMARKS_DIR=\"$(RBD_BENCHMARKS_PATH)\" -DRESULTS_DIR=\"$(RESULTS_PATH)\"
 #CXXFLAGS += -I$(RBDL_PATH) -I$(RBDL_PATH)/include -I$(EIGEN3_PATH)
 CXXFLAGS += -I$(RBDL_PATH) -I$(RBDL_PATH)/include 
@@ -11,13 +12,13 @@ CXXFLAGS += -I$(RBDL_PATH) -I$(RBDL_PATH)/include
 CXXFLAGS += -O0 -g -DDEBUG -std=c++11
 CXXFLAGS += -fno-omit-frame-pointer
 
-#x86:
-LDFLAGS += -L$(RBDL_PATH)/lib -lrbdl -lrbdl_urdfreader -lrt
-LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib
+##x86:
+#LDFLAGS += -L$(RBDL_PATH)/lib -lrbdl -lrbdl_urdfreader -lrt
+#LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib
 
 #RISC-V:
-# LDFLAGS += -L$(RBDL_PATH)/lib64 -lrbdl -lrbdl_urdfreader -lrt
-# LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib64
+ LDFLAGS += -L$(RBDL_PATH)/lib64 -lrbdl -lrbdl_urdfreader -lrt
+ LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib64
 
 
 $(TARGET): $(TARGET).o

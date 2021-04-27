a3be8979ec955641462249dce86417a9732c6a32
Cloud User <centos@ip-192-168-0-183.ec2.internal>
2021-04-27 22:44:21 +0000

    add single file for compilation

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Initializing submodules..."
 git submodule update --init --recursive
 mkdir -p ./csv/
 mkdir -p ./csv/rbdl/
 
 # Method obtained from https://dev.to/kojikanao/download-files-from-google-drive-with-curl-wget-5e4o
 # https://drive.google.com/u/1/uc?id=1AgACbjUpoWWA47U_RFR-jwIdt4lLTtdW&export=download
+echo "Downloading CSVs..."
 fileId=1AgACbjUpoWWA47U_RFR-jwIdt4lLTtdW
 fileName=csvs.zip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
@@ -15,4 +17,16 @@ curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code
 
 unzip "${fileName}" -d ./csv/rbdl/
 rm "${fileName}"
+
+echo "Building RBDL..."
+cd ./libs/
+make clean
+make rbdl
+cd ../
+
+echo "Building Experiments..."
+cd ./experiments/rbdl/
+./compile_rbdl_time.sh
+cd ../../
+
 

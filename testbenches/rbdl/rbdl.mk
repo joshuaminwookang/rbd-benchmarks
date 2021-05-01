d9d41c66f6d034e926471a91dd12782132ac0895
Cloud User <centos@ip-192-168-0-128.ec2.internal>
2021-05-01 02:04:04 +0000

    update build scripts to use staticly linked libraries for riscv baremetal execution

--- testbenches/rbdl/rbdl.mk
+++ testbenches/rbdl/rbdl.mk
@@ -1,12 +1,18 @@
 RM=rm -f
-RBD_RISCV_PATH=/root/rbd-benchmarks/
+#RBD_RISCV_PATH=/root/rbd-benchmarks/
+#RBD_BENCHMARKS_PATH=$(RBD_BENCHMARKS)
+#RBDL_PATH=$(RBD_BENCHMARKS_PATH)/libs/rbdl/install
+#EIGEN3_PATH=$(RBDL_PATH)/include/eigen3
+#RESULTS_PATH=$(RESULTS)
+
 RBD_BENCHMARKS_PATH=$(RBD_BENCHMARKS)
 RBDL_PATH=$(RBD_BENCHMARKS_PATH)/libs/rbdl/install
 EIGEN3_PATH=$(RBDL_PATH)/include/eigen3
 RESULTS_PATH=$(RESULTS)
 
-CXX=riscv64-unknown-linux-gnu-g++
-CXXFLAGS += -DRBD_BENCHMARKS_DIR=\"$(RBD_RISCV_PATH)\" -DRESULTS_DIR=\"$(RESULTS_PATH)\"
+#CXX=riscv64-unknown-linux-gnu-g++
+CXX=riscv64-unknown-elf-g++
+CXXFLAGS += -D_USE_MATH_DEFINES -DRBD_BENCHMARKS_DIR=\"$(RBD_BENCHMARKS_PATH)\" -DRESULTS_DIR=\"$(RESULTS_PATH)\"
 #CXXFLAGS += -DRBD_BENCHMARKS_DIR=\"$(RBD_BENCHMARKS_PATH)\" -DRESULTS_DIR=\"$(RESULTS_PATH)\"
 #CXXFLAGS += -I$(RBDL_PATH) -I$(RBDL_PATH)/include -I$(EIGEN3_PATH)
 CXXFLAGS += -I$(RBDL_PATH) -I$(RBDL_PATH)/include 
@@ -18,9 +24,13 @@ CXXFLAGS += -fno-omit-frame-pointer
 #LDFLAGS += -L$(RBDL_PATH)/lib -lrbdl -lrbdl_urdfreader -lrt
 #LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib
 
-#RISC-V:
- LDFLAGS += -L$(RBDL_PATH)/lib64 -lrbdl -lrbdl_urdfreader -lrt
- LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib64
+##RISC-V:
+# LDFLAGS += -L$(RBDL_PATH)/lib64 -lrbdl -lrbdl_urdfreader -lrt -lmath
+# LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib64
+
+#RISC-V Static:
+ LDFLAGS += -static -L$(RBDL_PATH)/lib -L$(RBDL_PATH)/bin -lrbdl -lrbdl_urdfreader 
+ LDFLAGS += -Wl,-rpath,$(RBDL_PATH)/lib
 
 
 $(TARGET): $(TARGET).o
